d341ac8658af52fd31e1658e8c2c1653c74acd16
iamgarvit <87112688+iamgarvit@users.noreply.github.com>
2024-10-19 16:54:35 +0530

    Add files via upload

--- AngryBirdsGame/core/src/main/java/com/baglogic/angrybirds/startScreen.java
+++ AngryBirdsGame/core/src/main/java/com/baglogic/angrybirds/startScreen.java
@@ -4,10 +4,21 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 
 public class startScreen extends ScreenAdapter {
     private final Core game;
     private Texture backgroundImage;
+    private Stage stage;
+    private Skin skin;
+    private TextButton startButton;
+    private TextButton exitButton;
 
     public startScreen(Core game) {
         this.game = game;
@@ -16,6 +27,38 @@ public class startScreen extends ScreenAdapter {
     @Override
     public void show() {
         backgroundImage = new Texture("bg5.png");
+        stage = new Stage(game.getViewport());
+
+        float screenWidth = Gdx.graphics.getWidth();
+        float screenHeight = Gdx.graphics.getHeight();
+
+        Gdx.input.setInputProcessor(stage);
+        skin = new Skin(Gdx.files.internal("data/uiskin.json"));
+
+        startButton = new TextButton("Start", skin);
+        startButton.setPosition(7f*screenWidth/8f,7f*screenHeight/8f);
+        startButton.setSize(screenWidth/4f, screenHeight/4f);
+
+        startButton.addListener(new ChangeListener() {
+            @Override
+            public void changed(ChangeEvent event, Actor actor) {
+                game.setScreen(game.levelChooseScreen);
+            }
+        });
+
+        exitButton = new TextButton("Exit", skin);
+        exitButton.setPosition(7f*screenWidth/8f, 7f*screenHeight/8f - screenHeight/2f);
+        exitButton.setSize(screenWidth/4f, screenHeight/4f);
+
+        exitButton.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                Gdx.app.exit();
+            }
+        });
+
+        stage.addActor(startButton);
+        stage.addActor(exitButton);
     }
 
     @Override
@@ -26,5 +69,8 @@ public class startScreen extends ScreenAdapter {
         game.getBatch().begin();
         game.getBatch().draw(backgroundImage, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
         game.getBatch().end();
+
+        stage.act(delta);
+        stage.draw();
     }
 }
