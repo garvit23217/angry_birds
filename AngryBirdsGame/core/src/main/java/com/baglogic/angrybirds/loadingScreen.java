5f91468e2d4e97569746fe5f8385cd6eda6904e7
iamgarvit <87112688+iamgarvit@users.noreply.github.com>
2024-10-27 10:57:32 +0530

    add bg music

--- AngryBirdsGame/core/src/main/java/com/baglogic/angrybirds/loadingScreen.java
+++ AngryBirdsGame/core/src/main/java/com/baglogic/angrybirds/loadingScreen.java
@@ -23,6 +23,7 @@ public class loadingScreen extends ScreenAdapter {
 
     @Override
     public void render(float delta) {
+        game.pauseBackgroundMusic();
         Gdx.gl.glClearColor(1, 1, 1, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
@@ -37,6 +38,7 @@ public class loadingScreen extends ScreenAdapter {
         time += delta;
 
         if (time > 3) {
+            game.playBackgroundMusic();
             game.setScreen(game.startScreen);
         }
     }
